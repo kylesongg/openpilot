c2686250150d3dd862dbb8b447b3265cd3ed87ab
kylesongg <133755770+kylesongg@users.noreply.github.com>
2023-05-24 19:02:12 +0900

    Update values.py
    remove fwdRadar

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -1705,11 +1705,11 @@ FW_VERSIONS = {
       b'\xf1\x00NQ5 FR_CMR AT USA LHD 1.00 1.00 99211-P1040 663',
       b'\xf1\x00NQ5 FR_CMR AT KOR LHD 1.00 1.00 99211-P1060 665',
     ],
-    (Ecu.fwdRadar, 0x7d0, None): [
-      b'\xf1\x00NQ5__               1.00 1.02 99110-P1000         ',
-      b'\xf1\x00NQ5__               1.00 1.03 99110-P1000         ',
-      b'\xf1\x00NQ5__               1.01 1.03 99110-P1000         ',
-    ],
+   # (Ecu.fwdRadar, 0x7d0, None): [
+   #   b'\xf1\x00NQ5__               1.00 1.02 99110-P1000         ',
+   #   b'\xf1\x00NQ5__               1.00 1.03 99110-P1000         ',
+   #   b'\xf1\x00NQ5__               1.01 1.03 99110-P1000         ',
+   # ],
   },
   CAR.GENESIS_GV70_1ST_GEN: {
     (Ecu.fwdCamera, 0x7c4, None): [
